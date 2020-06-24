fa0a4d651e2d89790e745111806ed7b2d5d2b227
arinabilous <bilousiryna6@gmail.com>
2020-06-24 13:21:42 +0300

    fff

---
+++ js/engine.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+        /***********************************/
+
+        $('.menu_btn').on('click', function () {
+            $('.navigation').toggleClass('navigation--scrolled');
+        });
+
+        /***********************************/
+
+    });
+
+}(jQuery));
