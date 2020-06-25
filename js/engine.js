ee4433aaa5c48b51c769522355787e013f7772d5
arinabilous <bilousiryna6@gmail.com>
2020-06-25 22:36:23 +0300

    changes

--- js/engine.js
+++ js/engine.js
@@ -8,6 +8,30 @@
             $('.navigation').toggleClass('navigation--scrolled');
         });
 
+
+        const svg = document.getElementById('svg-top-element');
+        const warp = new Warp(svg);
+        warp.interpolate(8);
+        warp.transform(([x, y]) => [x, y, y]);
+        let offset = 0;
+        const svg_left = document.getElementById('svg-left-element');
+        const warp2 = new Warp(svg_left);
+        warp2.interpolate(4);
+        warp2.transform(([x, y]) => [x, y, y]);
+        function animate()
+        {
+            warp.transform(([x, y, oy]) => [x, oy + 4 * Math.sin(x / 380 + offset), oy]);
+            warp2.transform(([x, y, oy]) => [x, oy + 8 * Math.sin(x / 192 + offset), oy]);
+            offset += 0.06;
+            requestAnimationFrame(animate);
+        }
+        animate();
+
+
+
+
+
+
         /***********************************/
 
     });
