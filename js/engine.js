0d677782feabe0890e1ad6c408cc4dd20007cd65
arinabilous <bilousiryna6@gmail.com>
2020-06-28 00:47:49 +0300

    changes suturday

--- js/engine.js
+++ js/engine.js
@@ -18,18 +18,28 @@
         const warp2 = new Warp(svg_left);
         warp2.interpolate(4);
         warp2.transform(([x, y]) => [x, y, y]);
-        function animate()
-        {
+
+        function animate() {
             warp.transform(([x, y, oy]) => [x, oy + 4 * Math.sin(x / 380 + offset), oy]);
             warp2.transform(([x, y, oy]) => [x, oy + 8 * Math.sin(x / 192 + offset), oy]);
             offset += 0.06;
             requestAnimationFrame(animate);
         }
+
         animate();
 
 
-
-
+        $('.review_slider').slick({
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            arrows: false,
+            infinite: true,
+            autoplay: false,
+            autoplaySpeed: 7000,
+            speed: 1000
+        });
 
 
         /***********************************/
